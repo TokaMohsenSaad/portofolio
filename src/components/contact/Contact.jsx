57035e075fe12635bbeba33e4e45235571f17e4e
Toka Mohsen <tokasaad2012@gmail.com>
2024-12-23 20:33:44 +0200

    hopefully done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                         <h3 className="contact__card-title">Messenger</h3>
                         <span className="contact__card-data">user</span>
 
-                        <a href="#" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
+                        <a href="/" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
                     </div>
                 </div>
             </div>
